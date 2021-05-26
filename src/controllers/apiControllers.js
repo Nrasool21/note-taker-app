aa29c4334e60cf2762e16ec8c5da1bd49f24428c
Nazia R <nazia.huma@gmail.com>
2021-05-27 00:51:39 +0100

    wip apiControllers.js

--- src/controllers/apiControllers.js
+++ src/controllers/apiControllers.js
@@ -1,23 +1,31 @@
 const { v4: uuidv4 } = require("uuid");
 const path = require("path");
 const fs = require("fs");
+const { title } = require("process");
+const { text } = require("express");
 //const cors = require("cors");
-
+//const dbJson = require("../../db/db.json");
 const BASE_URL = "http://localhost:3000/api/notes";
 
 //app.use(cors());
 
-
 const getAllNotes = (req, res) => {
   console.log("Welcome");
-  const filePath = path.join(__dirname, "/db/db.json");
-  const getAllNotesFromDb = fs.readFileSync("filePath", "UTF-8");
-  res.json(JSON.parse(getAllNotesFromDb));
+  const allNote = readFromDatabase();
+  //const filePath = path.join(__dirname, "../../db/db.json");
+  //const getAllNotesFromDb = fs.readFileSync("filePath", "UTF-8");
+  res.json(JSON.parse(dbNotes));
 };
 
 const saveNotes = (req, res) => {
-  let newId = uuidv4();
-  res.json({ message: newId });
+  const { message } = req.body;
+  const id = uuidv4();
+  const newNotes = {
+    title,
+    text,
+    id,
+  }
+  res.json({ message: "dinner at 7" });
 };
 
 const deleteNotes = (req, res) => {
@@ -26,8 +34,8 @@ const deleteNotes = (req, res) => {
 
 const getNotesById = (req, res) => {
   const { id } = req.params;
-
-  res.json({ message: "bye" });
+  const allNote = readFromDatabase();
+  res.json({ message: "Hello there" });
 };
 
 module.exports = { getAllNotes, saveNotes, deleteNotes, getNotesById };
